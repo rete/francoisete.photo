6e1cff940a527e91fdeac0971720e5f4ae748ad2
Remi Ete <remi.ete@gmail.com>
2020-03-27 21:39:34 +0100

    Changed deployement path on FTP

--- .travis-ci.d/deploy.sh
+++ .travis-ci.d/deploy.sh
@@ -2,6 +2,6 @@
 test ${TRAVIS_FTP_USER:?}
 test ${TRAVIS_FTP_PWD:?}
 
-ncftpput -R -v -u "${TRAVIS_FTP_USER}" -p ${TRAVIS_FTP_PWD} ftp.cluster021.hosting.ovh.net /public_html ./*
+ncftpput -R -v -u "${TRAVIS_FTP_USER}" -p ${TRAVIS_FTP_PWD} ftp.cluster021.hosting.ovh.net /www_new ./*
 
 exit $?
